7f8efb8fdca3f2ecd50e805919f6c0687189caa0
pablormacia <pr.macia@gmail.com>
2025-10-14 20:40:49 -0300

    Clase 2 - Estado, conceptos básicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter,setCounter] = useState(0)
+  const [nombre, setNombre] = useState("Pablo")
 
+  //console.log("counter:", counter)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Contador: {counter}</p>
+      <p>Nombre: {nombre}</p>
+      <button onClick={()=>setCounter(counter+1)}>Incrementar</button>
+      <button onClick={()=>setNombre("Rodolfo")}>Cambiar nombre</button>
+      {/* <a href="/otrapagina">Ir a otra página</a> */}
     </>
   )
 }
